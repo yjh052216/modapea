0c8581d9b80b28919ec4490777fb1b225e61b28f
yjh052216 <yjh052216@gmail.com>
2019-10-30 21:33:03 +0900

    w09

---
+++ w09-001/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,50 @@
+package com.springbook.biz.board.impl;
+
+import java.util.List;
+
+import com.springbook.biz.board.BoardDAO;
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+
+public class BoardServiceImpl implements BoardService {
+	
+	private BoardDAO boardDAO;
+	
+	public BoardDAO getBoardDAO() {
+		return boardDAO;
+	}
+
+	public void setBoardDAO(BoardDAO boardDAO) {
+		this.boardDAO = boardDAO;
+	}
+
+	public void insertBoard(BoardVO vo){
+		
+		this.boardDAO.insertBoard(vo);
+		
+	}
+	
+	public void updateBoard(BoardVO vo){
+		
+		this.boardDAO.updateBoard(vo);
+		
+	}
+	
+	public void deleteBoard(BoardVO vo){
+		
+		this.boardDAO.deleteBoard(vo);
+		
+	}
+	
+	public BoardVO getBoard(BoardVO vo){
+		
+		return this.boardDAO.getBoard(vo);
+		
+	}
+	
+	public List getBoardList(){
+		
+		return this.boardDAO.getBoardList();
+	}
+	
+}
